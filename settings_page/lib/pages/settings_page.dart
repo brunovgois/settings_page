f510737c0e1dd5b9f15904c8a620ba1d096391b6
Bruno Gois <brunovgois@gmail.com>
2021-04-09 00:29:36 -0300

    settings page locally changing color and font size

--- settings_page/lib/pages/settings_page.dart
+++ settings_page/lib/pages/settings_page.dart
@@ -1,16 +1,105 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({Key key}) : super(key: key);
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  double fontSize = 16;
+  List<int> colors = [0xFF455A64, 0xFFFFC107, 0xFF673AB7, 0xFF795548];
+  int selectedColor = 0xff1976d2;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Container(
-          child: Text('Settings Page'),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Text(
+                  'Set the App font size',
+                  style: TextStyle(
+                    fontSize: fontSize,
+                  ),
+                ),
+                Slider(
+                  activeColor: Color(selectedColor),
+                  label: fontSize.round().toString(),
+                  min: 12,
+                  max: 44,
+                  divisions: 5,
+                  value: fontSize,
+                  onChanged: (double newValue) {
+                    setState(() {
+                      fontSize = newValue;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Divider(),
+            Column(
+              children: [
+                Text(
+                  'Set the App main color',
+                  style: TextStyle(
+                    fontSize: fontSize,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: () => setColor(0),
+                      child: ColorCircle(colors[0]),
+                    ),
+                    GestureDetector(
+                      onTap: () => setColor(1),
+                      child: ColorCircle(colors[1]),
+                    ),
+                    GestureDetector(
+                      onTap: () => setColor(2),
+                      child: ColorCircle(colors[2]),
+                    ),
+                    GestureDetector(
+                      onTap: () => setColor(3),
+                      child: ColorCircle(colors[3]),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
         ),
+      ),
+    );
+  }
+
+  void setColor(int colorIndex) {
+    setState(() {
+      selectedColor = colors[colorIndex];
+    });
+  }
+}
+
+class ColorCircle extends StatelessWidget {
+  final int color;
+  ColorCircle(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(16),
+        ),
+        color: Color(color),
       ),
     );
   }
